760e994b1e98f2a5c89b2909bea7dac54bd2671e
primesourcellp <134374966+rselva001@users.noreply.github.com>
2025-08-18 11:56:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+// import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, Routes, HashRouter as Router } from 'react-router-dom';
+
 import About from './component/About/About.jsx';
 import Career from './component/Career/Career.jsx';
 import Contact from './component/Contact_us/Contact.jsx';
@@ -18,25 +20,23 @@ import Digital_Marketing from './component/Services/Digital_Marketing';
 
 export default function App() {
   return (
-    
     <Router>
-     
       <Navbar />
       <Routes>
-        <Route path="/Home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Services" element={<Services />} />
-          <Route path="/Services/Development" element={<Development />} />
-          <Route path="/Services/HR_consulting" element={<HR_consulting />} />
-          <Route path="/Services/PayRoll" element={< PayRoll/>} />
-          <Route path="/Services/Global" element={< Global/>} />
-          <Route path="/Services/Staffing" element={< Staffing/>} />
-          <Route path="/Services/Digital_Marketing" element={< Digital_Marketing/>} />
+        <Route path="/Services/Development" element={<Development />} />
+        <Route path="/Services/HR_consulting" element={<HR_consulting />} />
+        <Route path="/Services/PayRoll" element={<PayRoll />} />
+        <Route path="/Services/Global" element={<Global />} />
+        <Route path="/Services/Staffing" element={<Staffing />} />
+        <Route path="/Services/Digital_Marketing" element={<Digital_Marketing />} />
         <Route path="/Career" element={<Career />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Testimonial" element={<Testimonial />} />
-        <Route path="/Job-seeker" element={< JobOpening/>} />
+        <Route path="/Job-seeker" element={<JobOpening />} />
       </Routes>
       <Footer />
       <Copy />
